1c7847bb3e5d39fd26e2af2f862d711489409792
windiiswahyuni19 <iswahyuniwindi@gmail.com>
2025-09-01 10:18:31 +0900

    Create latihan2.dart

---
+++ DART/latihan2.dart
@@ -0,0 +1,12 @@
+void main(){
+  //menghitung luas dan keliling
+  int panjang= 10;
+  int lebar= 5;
+
+  int luas = panjang*lebar;
+  int keliling = 2*(panjang+lebar);
+  print ("panjang: $panjang");
+  print ("lebar: $lebar");
+  print ("luas: $luas");
+  print ("Keliling: $keliling"); 
+}
